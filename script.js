4023edc95fd655747a0c510240a6c1c4ef4b55a3
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-09 12:27:15 -0500

    fixed randomizer, each selected array is now random, pushed into an empty array, and randomized once again.  Tested various combinations of selections sucessfully.

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 
   function generatePassword() {
+
+  //defining characters with arrays
+ 
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+
+
     var characterSelect = prompt("How many characters do you want your password to have (please select a number between 8 and 128)?")
     if (isNaN(characterSelect)) {
       alert("You did not enter a valid number, please try again.")
@@ -21,31 +30,40 @@ var generateBtn = document.querySelector("#generate");
     var selectNumeric = confirm("Do you want to include numeric?")
     var selectSpecial = confirm("Do you want to include special characters?")
 
-  //defining characters with arrays
- 
-    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
-
   //user choices will be entered here (storage container)
 
     var recordKeeper = []
 
     if (selectLowercase === true) {
-      recordKeeper.push(lowercase)
+      for (var i = 0; i < lowercase.length; i++) {
+      var lowercaseRandom = lowercase[Math.floor(Math.random()*lowercase.length)];
+      recordKeeper.push(lowercaseRandom);
+      console.log(recordKeeper)
+      }
     }
 
     if (selectUppercase === true) {
-      recordKeeper.push(uppercase)
+      for (var i = 0; i < uppercase.length; i++) {
+      var uppercaseRandom = uppercase[Math.floor(Math.random()*uppercase.length)];
+      recordKeeper.push(uppercaseRandom);
+      console.log(recordKeeper)
+      }
     }
 
     if (selectNumeric === true) {
-      recordKeeper.push(numeric)
+      for (var i = 0; i < numeric.length; i++) {
+      var numericRandom = numeric[Math.floor(Math.random()*numeric.length)];
+      recordKeeper.push(numericRandom);
+      console.log(recordKeeper)
+      }
     }
 
     if (selectSpecial === true) {
-      recordKeeper.push(special)
+      for (var i = 0; i < special.length; i++) {
+      var specialRandom = special[Math.floor(Math.random()*special.length)];
+      recordKeeper.push(specialRandom);
+      console.log(recordKeeper)
+      }
     }
 
     if (recordKeeper.length === 0) {
@@ -55,19 +73,18 @@ var generateBtn = document.querySelector("#generate");
     var createdCode = ""
 
     for (var i = 0; i < characterSelect; i++) {
-      var randomAnswer = recordKeeper[Math.floor(Math.random()*recordKeeper.length)]
-      createdCode += randomAnswer
+      var randomAnswer = recordKeeper[Math.floor(Math.random() * recordKeeper.length)];
+      createdCode += randomAnswer;
     }
 
-  //if not selection is made, alert the user that at least one selection is required then prompt each question again.
-    
-  if (!lowercase && !uppercase && !numeric && !special) {
-      alert("You are required to make at least one selection")
-      lowercase = confirm("Do you want to include lowercase characters?");
-      uppercase = confirm("Do you want to include uppercase characters?");
-      numeric = confirm("Do you want to include numeric?");
-      special = confirm("Do you want to include special characters?");
-    }
+
+   if (!lowercase && !uppercase && !numeric && !special) {
+    alert("You are required to make at least one selection")
+    lowercase = confirm("Do you want to include lowercase characters?");
+    uppercase = confirm("Do you want to include uppercase characters?");
+    numeric = confirm("Do you want to include numeric?");
+    special = confirm("Do you want to include special characters?");
+  }
 
   return createdCode
 }
